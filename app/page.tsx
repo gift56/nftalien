bb6cfe575900a7234700b1d3abab2877a1f1dc4a
Efe G <efeasiughu@gmail.com>
2024-05-16 19:05:55 -0400

    done working on hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import How from "@/components/How";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <How />
     </main>
   );
 }
