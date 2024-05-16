0a6c7e8b33ac01c1de7ea28290fc84e099c9c2e0
Efe G <efeasiughu@gmail.com>
2024-05-16 18:09:41 -0400

    now getting the images

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,30 +11,49 @@ const navLinks = [
     text: "Home",
     href: "/",
   },
- 
+
   {
     text: "About",
     href: "/",
   },
- 
+
   {
     text: "roadmap",
     href: "/",
   },
- 
+
   {
     text: "COLLECTION",
     href: "/",
   },
- 
+
   {
     text: "FAQS",
     href: "/",
   },
- 
 ];
 
 const Header = () => {
+  const [mobileNav, setMobileNav] = useState<boolean>(false);
+
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside: EventListener = (event) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        setMobileNav(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <header className="W-full bg-bodydark sticky">
       <div className="contain">
