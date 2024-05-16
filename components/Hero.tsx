6721c93646447c1e363eb42c590fc335fb640a23
Efe G <efeasiughu@gmail.com>
2024-05-16 18:51:26 -0400

    working on hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,18 @@
+import Image from "next/image";
+
 const Hero = () => {
-  return <section>Hero</section>;
+  return (
+    <section className="min-h-screen  relative flex flex-col items-start justify-end">
+      <Image
+        src="/images/heroImage.png"
+        alt="nftalien hero"
+        width={1000}
+        height={1000}
+        priority
+        className="!w-full !h-full !object-cover"
+      />
+    </section>
+  );
 };
 
 export default Hero;
