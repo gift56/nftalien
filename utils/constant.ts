cedba0b9600507e66a203ee544c0562d6c6fbbc4
Efe G <efeasiughu@gmail.com>
2024-05-16 18:40:27 -0400

    working on hero

---
+++ utils/constant.ts
@@ -0,0 +1,18 @@
+export const howData = [
+  {
+    icon: "/icons/howIcon1.png",
+    title: "Connect Your Wallet",
+  },
+  {
+    icon: "/icons/howIcon2.png",
+    title: "Select Your Quaility",
+  },
+  {
+    icon: "/icons/howIcon3.png",
+    title: "Confirm the Transaction",
+  },
+  {
+    icon: "/icons/howIcon4.png",
+    title: "receive Your NFTs",
+  },
+];
