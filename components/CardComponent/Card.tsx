3e0130ac0eb080ca4a8c949d035416fb72e28043
Efe G <efeasiughu@gmail.com>
2024-05-16 19:42:53 -0400

    done working on hero

--- components/CardComponent/Card.tsx
+++ components/CardComponent/Card.tsx
@@ -1,5 +1,9 @@
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="w-full bg-dark relative before:content-[''] before:absolute before:w-2/4 before:h-[180%]">
+      {children}
+    </div>
+  );
 };
 
 export default Card;
--- components/How.tsx
+++ components/How.tsx
@@ -1,3 +1,7 @@
+import { howData } from "@/utils/constant";
+import Card from "./CardComponent/Card";
+import Image from "next/image";
+
 const How = () => {
   return (
     <section className="w-full py-12">
@@ -6,7 +10,22 @@ const How = () => {
           <h2 className="font-bak text-xl text-center md:text-2xl lg:text-4xl font-normal">
             HOW TO <span className="font-bak text-primary">NFTALIEN</span> WORK
           </h2>
-          <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 items-start justify-start gap-5"></div>
+          <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 items-start justify-start gap-5">
+            {howData.map((item, index) => (
+              <Card key={index}>
+                <div className="w-full py-5 px-4 flex flex-col items-center justify-center gap-4 h-[210px]">
+                  <Image
+                    src={item.icon}
+                    alt="how icon"
+                    width={82}
+                    height={72}
+                    priority
+                    className="!w-fit !h-fit"
+                  />
+                </div>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </section>
