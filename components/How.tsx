8fe6c02e23fa8995394bb0bea55f3720148192b4
Efe G <efeasiughu@gmail.com>
2024-05-16 19:33:33 -0400

    done working on hero

--- components/How.tsx
+++ components/How.tsx
@@ -6,6 +6,7 @@ const How = () => {
           <h2 className="font-bak text-xl text-center md:text-2xl lg:text-4xl font-normal">
             HOW TO <span className="font-bak text-primary">NFTALIEN</span> WORK
           </h2>
+          <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 items-start justify-start gap-5"></div>
         </div>
       </div>
     </section>
