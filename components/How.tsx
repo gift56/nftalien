49cb5e0f95987766bd28fc8918bcd0c81b51ff50
Efe G <efeasiughu@gmail.com>
2024-05-16 19:32:36 -0400

    done working on hero

--- components/How.tsx
+++ components/How.tsx
@@ -3,7 +3,9 @@ const How = () => {
     <section className="w-full py-12">
       <div className="contain">
         <div className="w-full flex flex-col gap-6 items-center justify-center">
-          <h2>HOW TO NFTALIEN WORK</h2>
+          <h2 className="font-bak text-xl text-center md:text-2xl lg:text-4xl font-normal">
+            HOW TO <span className="font-bak text-primary">NFTALIEN</span> WORK
+          </h2>
         </div>
       </div>
     </section>
