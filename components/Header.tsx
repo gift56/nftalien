e8b7a3d7f5a9294d70373cfaae61b32dc7063bb4
Efe G <efeasiughu@gmail.com>
2024-05-16 18:06:40 -0400

    now getting the images

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,34 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+import { MdClose } from "react-icons/md";
+import { FaBars } from "react-icons/fa";
+
+const navLinks = [
+  {
+    text: "Creon Pass",
+    href: "pass",
+    soonText: false,
+  },
+  {
+    text: "Token",
+    href: "token",
+    soonText: true,
+  },
+  {
+    text: "AI Revenue",
+    href: "revenue",
+    soonText: true,
+  },
+  {
+    text: "AI Launchpad",
+    href: "launchpad",
+    soonText: true,
+  },
+];
+
 const Header = () => {
   return (
     <header className="W-full bg-bodydark sticky">
