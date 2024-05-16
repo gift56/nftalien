b7c0596ff077bc11b810661645c6c09492c82c9c
Efe G <efeasiughu@gmail.com>
2024-05-16 19:30:57 -0400

    done working on hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <section className="min-h-screen relative flex flex-col items-center justify-end pb-10">
       <div className="absolute -top-16 w-full h-full">
         <Image
-          src="/images/heroImage.png"
+          src="/images/heroImage.webp"
           alt="nftalien hero"
           width={1000}
           height={1000}
--- components/How.tsx
+++ components/How.tsx
@@ -1,5 +1,13 @@
 const How = () => {
-  return <section>How</section>;
+  return (
+    <section className="w-full py-12">
+      <div className="contain">
+        <div className="w-full flex flex-col gap-6 items-center justify-center">
+          <h2>HOW TO NFTALIEN WORK</h2>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default How;
