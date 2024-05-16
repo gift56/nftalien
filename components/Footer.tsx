ca7ba13078a2d312563c4e6ed6f787af62266e12
Efe G <efeasiughu@gmail.com>
2024-05-16 18:49:02 -0400

    working on hero

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,29 @@
+import Image from "next/image";
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="bg-bodydark">
+      <div className="contain">
+        <div className="w-full flex flex-col md:flex-row items-center justify-between gap-5">
+          <div className="w-full md:w-fit flex flex-col md:flex-row items-start justify-start gap-3 md:gap-6">
+            <p className="text-base font-normal text-white font-satoshi">
+              © NFTALIEN 2024. All rights reserved.
+            </p>
+          </div>
+          <p className="text-base font-normal text-white font-satoshi flex items-center gap-1">
+            <span>Powered by</span>
+            <Image
+              src="/images/logo.png"
+              alt="nftables logo"
+              width={101}
+              height={18}
+              priority
+            />
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
