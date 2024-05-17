3294248c0e068c377f9f27e4180653124b67afda
Efe G <efeasiughu@gmail.com>
2024-05-17 07:35:13 -0400

    working on collection section

--- components/Collections.tsx
+++ components/Collections.tsx
@@ -2,10 +2,13 @@ const Collections = () => {
   return (
     <section className="w-full py-12">
       <div className="contain">
-        <div className="w-full flex flex-col gap-8 items-center justify-center">
+        <div className="w-full flex flex-col gap-8 items-start justify-start">
           <h2 className="font-bak text-xl text-center md:text-2xl lg:text-4xl font-normal">
-            HOW TO <span className="font-bak text-primary">NFTALIEN</span> WORK
+            HOT COLLECTION
           </h2>
+          <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 items-start justify-start gap-5">
+            
+          </div>
         </div>
       </div>
     </section>
