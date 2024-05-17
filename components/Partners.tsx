c473383fa68c4dc6c5e2a99fc05b1d140652fa8b
Efe G <efeasiughu@gmail.com>
2024-05-17 08:45:51 -0400

    working on story section

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -3,7 +3,7 @@ const Partners = () => {
     <section className="w-full py-12">
       <div className="contain">
         <div className="w-full flex flex-col gap-8 items-center justify-center">
-          
+        <h2 className="font-bak text-xl text-center md:text-2xl lg:text-4xl font-normal"></h2>
         </div>
       </div>
     </section>
