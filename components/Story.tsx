0f1abea1a675a2142dec62fd9a74655fd33441b4
Efe G <efeasiughu@gmail.com>
2024-05-17 08:02:19 -0400

    working on story section

--- components/Story.tsx
+++ components/Story.tsx
@@ -1,5 +1,11 @@
 const Story = () => {
-  return <section className="w-full py-12">Story</section>;
+  return (
+    <section className="w-full py-12">
+      <div className="contain">
+        
+      </div>
+    </section>
+  );
 };
 
 export default Story;
