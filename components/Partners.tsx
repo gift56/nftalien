76482644b0d12e9605392dc50a77c5569c90600e
Efe G <efeasiughu@gmail.com>
2024-05-17 08:48:34 -0400

    working on story section

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -1,3 +1,5 @@
+import { partnerData } from "@/utils/constant";
+
 const Partners = () => {
   return (
     <section className="w-full py-12">
@@ -7,7 +9,9 @@ const Partners = () => {
             <span className="font-bak text-primary">BINABOX</span> pARTNER
           </h2>
           <div className="w-full grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 items-start justify-start gap-3">
-
+            {partnerData.map((item, index) => (
+              <div></div>
+            ))}
           </div>
         </div>
       </div>
