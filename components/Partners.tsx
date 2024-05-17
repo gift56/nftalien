8ac18448fb37d93c55d8a482a81c065ecb930ddd
Efe G <efeasiughu@gmail.com>
2024-05-17 08:45:37 -0400

    working on story section

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -2,7 +2,9 @@ const Partners = () => {
   return (
     <section className="w-full py-12">
       <div className="contain">
-        
+        <div className="w-full flex flex-col gap-8 items-center justify-center">
+          
+        </div>
       </div>
     </section>
   );
