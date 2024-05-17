9d5a82fbbeda9e8c09600dd05271479c95be94ff
Efe G <efeasiughu@gmail.com>
2024-05-17 09:06:42 -0400

    addming some animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { MotionContainer } from "./MotionContainer";
 
 const Hero = () => {
   return (
@@ -14,27 +15,48 @@ const Hero = () => {
         />
       </div>
       <div className="contain relative z-10 flex flex-col gap-5 items-center justify-center">
-        <h1 className="text-white text-center text-3xl md:text-5xl lg:text-[62px] lg:leading-[74.8px] font-normal font-bak uppercase">
-          EXPLORE NFT COLLECTION
-        </h1>
-        <p className="text-lg md:text-xl font-normal md:max-w-[752px] text-center">
-          A collection of 2525 highly-fashionable NFTs on the ETH Blockchain.
-          Unique, metaverse-ready, and designed to benefit their holders.
-        </p>
-        <div className="flex w-full items-center justify-center gap-5">
-          <button
-            type="button"
-            className="w-fit px-3 py-3 md:px-6 flex items-center justify-center gap-3 bg-primary rounded text-xs md:text-base font-bak font-normal uppercase text-dark hover:opacity-80 transition-all duration-300"
-          >
-            connect wallet
-          </button>
-          <button
-            type="button"
-            className="w-fit px-3 py-3 md:px-6 flex items-center justify-center gap-3 border-2 border-primary rounded text-xs md:text-base font-bak font-normal uppercase text-white hover:opacity-80 transition-all duration-300"
-          >
-            whitelist now
-          </button>
-        </div>
+        <MotionContainer
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{ type: "spring", duration: 1 }}
+        >
+          <h1 className="text-white text-center text-3xl md:text-5xl lg:text-[62px] lg:leading-[74.8px] font-normal font-bak uppercase">
+            EXPLORE NFT COLLECTION
+          </h1>
+        </MotionContainer>
+        <MotionContainer
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ type: "spring", duration: 1, delay: 0.1 }}
+        >
+          <p className="text-lg md:text-xl font-normal md:max-w-[752px] text-center">
+            A collection of 2525 highly-fashionable NFTs on the ETH Blockchain.
+            Unique, metaverse-ready, and designed to benefit their holders.
+          </p>
+        </MotionContainer>
+        <MotionContainer
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{ type: "spring", duration: 1, delay: 0.2 }}
+        >
+          <div className="flex w-full items-center justify-center gap-5">
+            <button
+              type="button"
+              className="w-fit px-3 py-3 md:px-6 flex items-center justify-center gap-3 bg-primary rounded text-xs md:text-base font-bak font-normal uppercase text-dark hover:opacity-80 transition-all duration-300"
+            >
+              connect wallet
+            </button>
+            <button
+              type="button"
+              className="w-fit px-3 py-3 md:px-6 flex items-center justify-center gap-3 border-2 border-primary rounded text-xs md:text-base font-bak font-normal uppercase text-white hover:opacity-80 transition-all duration-300"
+            >
+              whitelist now
+            </button>
+          </div>
+        </MotionContainer>
       </div>
     </section>
   );
