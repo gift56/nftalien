837c7d19f33498fcdc6b75610de49eb729083a7c
Efe G <efeasiughu@gmail.com>
2024-05-17 08:48:10 -0400

    working on story section

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -6,6 +6,9 @@ const Partners = () => {
           <h2 className="font-bak text-xl text-center md:text-2xl lg:text-4xl font-normal uppercase">
             <span className="font-bak text-primary">BINABOX</span> pARTNER
           </h2>
+          <div className="w-full grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 items-start justify-start gap-3">
+
+          </div>
         </div>
       </div>
     </section>
