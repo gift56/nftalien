128cfb0de1d8bf6a1528a038317d4cb4af1c4274
Efe G <efeasiughu@gmail.com>
2024-05-17 08:46:43 -0400

    working on story section

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -3,7 +3,9 @@ const Partners = () => {
     <section className="w-full py-12">
       <div className="contain">
         <div className="w-full flex flex-col gap-8 items-center justify-center">
-        <h2 className="font-bak text-xl text-center md:text-2xl lg:text-4xl font-normal"></h2>
+          <h2 className="font-bak text-xl text-center md:text-2xl lg:text-4xl font-normal uppercase">
+            <span className="font-bak text-primary">BINABOX</span> pARTNER
+          </h2>
         </div>
       </div>
     </section>
