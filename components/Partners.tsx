ad1ac6ce512631191f42ea34738b5f9ade8250a7
Efe G <efeasiughu@gmail.com>
2024-05-17 09:19:10 -0400

    adding some animation

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -1,18 +1,42 @@
 import { partnerData } from "@/utils/constant";
 import Image from "next/image";
+import { MotionContainer } from "./MotionContainer";
 
 const Partners = () => {
+  const stagger = 0.25;
+
+  const variants = {
+    hidden: { opacity: 0, scale: 0.5 },
+    visible: { opacity: 1, scale: 1 },
+  };
+
   return (
     <section className="w-full py-12">
       <div className="contain">
         <div className="w-full flex flex-col gap-8 items-center justify-center">
-          <h2 className="font-bak text-xl text-center md:text-2xl lg:text-4xl font-normal uppercase">
-            <span className="font-bak text-primary">BINABOX</span> pARTNER
-          </h2>
+          <MotionContainer
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ type: "spring", duration: 1 }}
+          >
+            <h2 className="font-bak text-xl text-center md:text-2xl lg:text-4xl font-normal uppercase">
+              <span className="font-bak text-primary">BINABOX</span> pARTNER
+            </h2>
+          </MotionContainer>
           <div className="w-full grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 items-start justify-start gap-3">
             {partnerData.map((item, index) => (
-              <div
+              <MotionContainer
                 key={index}
+                variants={variants}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                transition={{
+                  delay: index * stagger,
+                  ease: "easeInOut",
+                  duration: 0.5,
+                }}
                 className="w-full h-[80px] hover:bg-dark flex items-center justify-center transition-all duration-300"
               >
                 <Image
@@ -22,7 +46,7 @@ const Partners = () => {
                   height={60}
                   priority
                 />
-              </div>
+              </MotionContainer>
             ))}
           </div>
         </div>
--- components/Story.tsx
+++ components/Story.tsx
@@ -7,7 +7,7 @@ const Story = () => {
 
   const variants = {
     hidden: { opacity: 0, scale: 0.5 },
-    visible: { opacity: 1, sclae: 1 },
+    visible: { opacity: 1, scale: 1 },
   };
 
   return (
