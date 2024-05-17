4edcf1d8dc488821af520f1b957b17bb5e78fba2
Efe G <efeasiughu@gmail.com>
2024-05-17 07:29:11 -0400

    done working on how

--- components/How.tsx
+++ components/How.tsx
@@ -5,7 +5,7 @@ const How = () => {
   return (
     <section className="w-full py-12">
       <div className="contain">
-        <div className="w-full flex flex-col gap-6 items-center justify-center">
+        <div className="w-full flex flex-col gap-8 items-center justify-center">
           <h2 className="font-bak text-xl text-center md:text-2xl lg:text-4xl font-normal">
             HOW TO <span className="font-bak text-primary">NFTALIEN</span> WORK
           </h2>
