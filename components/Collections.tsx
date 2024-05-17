87df82a9ac41de13ee2a79628842fa274e7fd9d7
Efe G <efeasiughu@gmail.com>
2024-05-17 09:10:45 -0400

    adding some animation

--- components/Collections.tsx
+++ components/Collections.tsx
@@ -3,17 +3,40 @@ import Image from "next/image";
 import { MotionContainer } from "./MotionContainer";
 
 const Collections = () => {
+  const stagger = 0.25;
+
+  const variants = {
+    hidden: { opacity: 0, y: 60 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
     <section className="w-full py-12">
       <div className="contain">
         <div className="w-full flex flex-col gap-8 items-start justify-start">
-          <h2 className="font-bak text-xl text-center md:text-2xl lg:text-4xl font-normal">
-            HOT COLLECTION
-          </h2>
+          <MotionContainer
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ type: "spring", duration: 1 }}
+          >
+            <h2 className="font-bak text-xl text-center md:text-2xl lg:text-4xl font-normal">
+              HOT COLLECTION
+            </h2>
+          </MotionContainer>
           <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 items-start justify-start gap-5">
             {collectionData.map((item, index) => (
-              <div
+              <MotionContainer
                 key={index}
+                variants={variants}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                transition={{
+                  delay: index * stagger,
+                  ease: "easeInOut",
+                  duration: 0.5,
+                }}
                 className="w-full py-4 px-2 flex flex-col items-start justify-start gap-4 h-[350px] relative overflow-hidden bg-dark before:hover:content-[''] before:hover:absolute before:hover:top-[-42%] before:left-[48%] before:hover:w-[20%] before:hover:h-[200%] before:hover:bg-cardLinear before:hover:rotate-[-40deg] before:transition-all before:duration-300 after:hover:content-[''] after:hover:absolute after:hover:inset-[5px] after:hover:bg-dark"
               >
                 <div className="relative z-10 w-full flex flex-col items-start justify-between gap-4">
@@ -29,7 +52,7 @@ const Collections = () => {
                     {item.title}
                   </h4>
                 </div>
-              </div>
+              </MotionContainer>
             ))}
           </div>
         </div>
